31021b838e30042a22a6b3e31bc6a3bb80b252a7
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-04-27 20:38:55 -0700

    Fix incorrect use of condition variables
    Waiting on the incorrect conditions to be true was
    causing a deadlock.

--- source/traffic/timeBasedPreemption.h
+++ source/traffic/timeBasedPreemption.h
@@ -47,7 +47,6 @@ namespace traffic {
 
         std::cout << std::this_thread::get_id() << "enterStreet getting mutex for " << directionToString(carDirection) << std::endl;
         std::unique_lock<std::mutex> streetLock(streetMutex);
-        std::cout << std::this_thread::get_id() << " enterStreet: " << numCarsOnStreet << " cars on street going " << directionToString(streetDirection) << std::endl;
 
         // if (numCarsOnStreet == 0 and changeDirection) {
         //     streetDirection = (streetDirection == Direction::EAST)? Direction::WEST : Direction::EAST;
@@ -56,14 +55,14 @@ namespace traffic {
         
         if (numCarsOnStreet < MAX_OCCUPANCY) { 
             if(carDirection == streetDirection) {   
-                std::cout << "there is some space on the street, drive on through" << std::endl;
+                std::cout << "enterStreet: there is some space on the street, only " << numCarsOnStreet << " cars on street going " << directionToString(streetDirection) << " drive on through" << std::endl;
                 driveThroughStreet(streetLock, carDirection);
             } else {
                 std::cout << "street in wrong direction, wait your turn" << std::endl;
                 waitToEnterStreet(streetLock, carDirection);
             }
         } else { 
-            std::cout << "street at max occupancy, wait your turn" << std::endl;
+            std::cout << "enterStreet: street at max occupancy, with "  << numCarsOnStreet << " cars on street going " << directionToString(streetDirection) << " wait your turn" << std::endl;
             waitToEnterStreet(streetLock, carDirection);
         }
     }
@@ -82,6 +81,7 @@ namespace traffic {
         }
 
         streetLock.unlock();
+        std::cout << std::this_thread::get_id() << "driveThroughStreet gives up lock" << std::endl;
 
         int j = 0;
         for (int i = 0; i < 100000; i++) {
@@ -95,13 +95,21 @@ namespace traffic {
         std::cout << std::this_thread::get_id() << " waitToEnterStreet in direction: " << directionToString(carDirection) << std::endl;
         if (carDirection ==  Direction::EAST) {
             while(numCarsOnStreet == MAX_OCCUPANCY  || carDirection != streetDirection) {
-                enterEastBound.wait(streetLock, [this]{ return canEnterEast; });
+                enterEastBound.wait(streetLock);
+                // std::cout << std::this_thread::get_id() << " waitToEnterStreet woke car direction " << directionToString(carDirection)
+                // << " street direction " << directionToString(streetDirection) << " occupancy " << numCarsOnStreet << std::endl;
             }
+            canEnterEast = false;
+            std::cout << std::this_thread::get_id() << " in waitToEnterStreet woke up and can go now in direction: " << directionToString(carDirection) << std::endl;
             driveThroughStreet(streetLock, carDirection);
         } else {
             while(numCarsOnStreet == MAX_OCCUPANCY  || carDirection != streetDirection) {
-                enterWestBound.wait(streetLock, [this]{ return canEnterWest; });
+                enterWestBound.wait(streetLock);
+                // std::cout << std::this_thread::get_id() << " waitToEnterStreet woke car direction " << directionToString(carDirection)
+                // << " street direction " << directionToString(streetDirection) << " occupancy " << numCarsOnStreet << std::endl;
             }
+            canEnterWest = false;
+            std::cout << std::this_thread::get_id() << " in waitToEnterStreet woke up and can go now in direction: " << directionToString(carDirection) << std::endl;
             driveThroughStreet(streetLock, carDirection);
         }
     }
@@ -118,18 +126,18 @@ namespace traffic {
             changeDirection = false;
             streetDirection = (streetDirection == Direction::EAST)? Direction::WEST : Direction::EAST;
         }
+
         if (numCarsOnStreet < MAX_OCCUPANCY) {
             signalCarToEnter(streetLock);
         }
     }
 
     void TimeBasedPreemption::signalCarToEnter(std::unique_lock<std::mutex>& streetLock) {
+        std::cout << std::this_thread::get_id() << "signalCarToEnter in direction " << directionToString(streetDirection) << std::endl;
         if (streetDirection == Direction::EAST) {
-            canEnterEast = true;
-            enterEastBound.notify_all();
+            enterEastBound.notify_one();
         } else {
-            canEnterWest = true;
-            enterWestBound.notify_all();
+            enterWestBound.notify_one();
         }
     }
 
@@ -149,8 +157,11 @@ namespace traffic {
 
         // // Wait until the street is empty before changing direction
         // while (numCarsOnStreet > 0) {
+        //     std::cout << std::this_thread::get_id() << "num cars on street " << numCarsOnStreet << std::endl;
         //     streetLock.unlock();
+        //     std::cout << std::this_thread::get_id() << "changeStreetDirection gives up lock" << std::endl;
         //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        //     std::cout << std::this_thread::get_id() << "changeStreetDirection re aquires lock" << std::endl;
         //     streetLock.lock();
         // }
 
